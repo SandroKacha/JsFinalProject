84805391f8778b247fe97120b4f40420d3452d2b
unknown <aleksandre.kacharava@gau.edu.ge>
2025-01-14 01:11:37 +0400

    JsFinalUpdate

---
+++ language.js
@@ -0,0 +1,220 @@
+const languageButtons = document.querySelector('.langDiv');
+const Element1 = document.getElementById('Ttext');
+const Element2 = document.getElementById('Ttext1');
+const Element3 = document.getElementById('Ttext2');
+const Element4 = document.getElementById('Ttext3');
+const Element5 = document.getElementById('Ttext4');
+const Element6 = document.getElementById('Ttext5');
+const Element7 = document.getElementById('Ttext6');
+const Element8 = document.getElementById('Ttext7');
+const Element9 = document.getElementById('Ttext8');
+const Element10 = document.getElementById('Ttext9');
+const Element11 = document.getElementById('Ttext10');
+const Element12 = document.getElementById('Ttext11');
+const Element13 = document.getElementById('Ttext12');
+const Element14 = document.getElementById('Ttext13');
+const Element15 = document.getElementById('Ttext14');
+const Element16 = document.getElementById('Ttext15');
+const Element17 = document.getElementById('Ttext16');
+const Element18 = document.getElementById('Ttext17');
+const Element19 = document.getElementById('Ttext18');
+const Element20 = document.getElementById('Ttext19');
+const Element21 = document.getElementById('Ttext20');
+const Element22 = document.getElementById('Ttext21');
+const Element23 = document.getElementById('Ttext22');
+const Element24 = document.getElementById('Ttext23');
+const Element25 = document.getElementById('Ttext24');
+const Element26 = document.getElementById('Ttext25');
+const Element27 = document.getElementById('Ttext26');
+const Element28 = document.getElementById('Ttext27');
+const Element29 = document.getElementById('Ttext28');
+const Element30 = document.getElementById('Ttext29');
+const Element31 = document.getElementById('Ttext30');
+const Element32 = document.getElementById('Ttext31');
+const Element33 = document.getElementById('Ttext32');
+const Element34 = document.getElementById('Ttext33');
+const Element35 = document.getElementById('Ttext34');
+const Element36 = document.getElementById('Ttext35');
+const Element37 = document.getElementById('Ttext36');
+const Element38 = document.getElementById('Ttext37');
+const Element39 = document.getElementById('Ttext38');
+const Element40 = document.getElementById('Ttext39');
+const Element41 = document.getElementById('Ttext40');
+const Element42 = document.getElementById('Ttext41');
+const Element43 = document.getElementById('Ttext42');
+const Element44 = document.getElementById('Ttext43');
+const Element45 = document.getElementById('Ttext44');
+const Element46 = document.getElementById('Ttext45');
+const Element47 = document.getElementById('Ttext46');
+
+
+const translations = {
+    "en": {
+        "Ttext": "Revolutionize Your Workspace  Discover high-performance computers.",
+        "Ttext1": "Let's go!",
+        "Ttext2": "Show me more",
+        "Ttext3": "Why Choose TechZone? For the next reasons!",
+        "Ttext4": "High Performance",
+        "Ttext5": "Experience unmatched speed, unparalleled reliability, and seamless performance with our state-of-the-art, cutting-edge technology designed to exceed your expectations.",
+        "Ttext6": "Wide Selection",
+        "Ttext7": "From high-performance gaming rigs to ergonomic peripherals designed for ultimate comfort and precision, we offer everything you need to elevate your setup.",
+        "Ttext8": "Global Delivery",
+        "Ttext9": "Get your favorite tech products delivered right to your doorstep, no matter where you are in the world, with fast and reliable shipping services.",
+        "Ttext10": "Order anything",
+        "Ttext11": "Order anything you need, anytime, and have it conveniently delivered to your location with just a few clicks.",
+        "Ttext12": "What we Offer",
+        "Ttext13": "Unleash your potential with our high-performance gaming laptops.",
+        "Ttext14": "Elevate your typing and gaming experience.",
+        "Ttext15": "Immerse yourself in crystal-clear audio.",
+        "Ttext16": "OUR CUSTOMERS CAN'T LIVE WITHOUT US",
+        "Ttext17": "TechZone's products are top-notch. My gaming setup has never been better!",
+        "Ttext18": "Incredible service and fast delivery. Highly recommend!",
+        "Ttext19": "The mechanical keyboard is a game-changer for my productivity.",
+        "Ttext20": "TechZone_Offers",
+        "Ttext21": "Premium",
+        "Ttext22": "399$ per month",
+        "Ttext23": "Just $13.30 per upgrade",
+        "Ttext24": "1 system upgrade every day",
+        "Ttext25": "24/7 access to our tech arsenal",
+        "Ttext26": "Priority access to the latest innovations",
+        "Ttext27": "Free delivery",
+        "Ttext28": "Pro",
+        "Ttext29": "149$ per month",
+        "Ttext30": "Just $14.90 per upgrade",
+        "Ttext31": "1 system upgrade for 10 days/month",
+        "Ttext32": "24/7 access to our tech arsenal",
+        "Ttext33": "Exclusive access to cutting-edge releases",
+        "Ttext34": "Free delivery",
+        "Ttext35": "Starter",
+        "Ttext36": "19$ per Month",
+        "Ttext37": "1 system upgrade included",
+        "Ttext38": "Order available from 8 AM to 12 PM",
+        "Ttext39": "",
+        "Ttext40": "Free delivery",
+        "Ttext41": "Info",
+        "Ttext42": "Products",
+        "Ttext43": "Offers",
+        "Ttext44": "Comments",
+        "Ttext45": "Subscribe",
+        "Ttext46": "Sign up",
+        
+ 
+    },
+    "ge": {
+        
+            "Ttext": "რევოლუცია მოახდინე შენს სამუშაო სივრცეში - აღმოაჩინე მაღალი წარმადობის კომპიუტერები.",
+            "Ttext1": "წავიდეთ!",
+            "Ttext2": "მაჩვენე მეტი",
+            "Ttext3": "რატომ აირჩიოთ TechZone? შემდეგი მიზეზების გამო!",
+            "Ttext4": "მაღალი წარმადობა",
+            "Ttext5": "გამოცადე შეუდარებელი სიჩქარე, უახლესი საიმედოობა და უნაკლო მუშაობა ჩვენი თანამედროვე, უახლესი ტექნოლოგიით, რომელიც აჭარბებს თქვენს მოლოდინს.",
+            "Ttext6": "ფართო არჩევანი",
+            "Ttext7": "მაღალი წარმადობის სათამაშო კომპიუტერებიდან დაწყებული კომფორტისა და სიზუსტისთვის შექმნილი ერგონომიული პერიფერიებით, ჩვენ ვთავაზობთ ყველაფერს, რაც საჭიროა თქვენი სამუშაო ადგილის გაუმჯობესებისთვის.",
+            "Ttext8": "გლობალური მიწოდება",
+            "Ttext9": "მიიღე შენი საყვარელი ტექნოლოგიური პროდუქტები პირდაპირ შენს კართან, მსოფლიოს ნებისმიერ წერტილში, სწრაფი და საიმედო მიწოდების მომსახურებით.",
+            "Ttext10": "შეუკვეთე ყველაფერი",
+            "Ttext11": "შეუკვეთე ყველაფერი, რაც გჭირდება, ნებისმიერ დროს და მიიღე ის მარტივად, რამდენიმე დაწკაპუნებით.",
+            "Ttext12": "რას ვთავაზობთ",
+            "Ttext13": "გამოავლინე შენი პოტენციალი ჩვენი მაღალი წარმადობის სათამაშო ლეპტოპებით.",
+            "Ttext14": "გააუმჯობესე შენი ტაიპინგისა და სათამაშო გამოცდილება.",
+            "Ttext15": "გახდი მთელყოფილი აუდიოს გამოცდილებით.",
+            "Ttext16": "ჩვენი მომხმარებლები ვერ ცხოვრობენ ჩვენ გარეშე",
+            "Ttext17": "TechZone-ის პროდუქცია უმაღლეს დონეზეა. ჩემი სათამაშო კომპლექტი არასდროს ყოფილა უკეთესი!",
+            "Ttext18": "შეუდარებელი მომსახურება და სწრაფი მიწოდება. ძალიან ვურჩევ!",
+            "Ttext19": "მექანიკური კლავიატურა საჩუქარი გახდა ჩემი პროდუქტიულობისთვის.",
+            "Ttext20": "TechZone_წარმოადგენს",
+            "Ttext21": "პრემიუმი",
+            "Ttext22": "399$ თვეში",
+            "Ttext23": "მხოლოდ $13.30 ყოველ განახლებაზე",
+            "Ttext24": "1 სისტემა განახლება ყოველ დღე",
+            "Ttext25": "24/7 წვდომა ჩვენი ტექნოლოგიური პორტფოლიოსთვის",
+            "Ttext26": "პრიორიტეტული წვდომა უახლეს ინოვაციებზე",
+            "Ttext27": "უფასო მიწოდება",
+            "Ttext28": "პროფესიონალი",
+            "Ttext29": "149$ თვეში",
+            "Ttext30": "მხოლოდ $14.90 ყოველ განახლებაზე",
+            "Ttext31": "1 სისტემა განახლება 10 დღე/თვეში",
+            "Ttext32": "24/7 წვდომა ჩვენი ტექნოლოგიური პორტფოლიოსთვის",
+            "Ttext33": "საერთაშორისო წვდომა უახლეს გამოსავლელებზე",
+            "Ttext34": "უფასო მიწოდება",
+            "Ttext35": "დასაწყისი",
+            "Ttext36": "19$ თვეში",
+            "Ttext37": "1 სისტემა განახლება შედის",
+            "Ttext38": "შეკვეთის მიღება შესაძლებელია 8 საათიდან 12 საათამდე",
+            "Ttext39": "",
+            "Ttext40": "უფასო მიწოდება",
+            "Ttext41": "ინფორმაცია",
+            "Ttext42": "პროდუქტები",
+            "Ttext43": "შეთავაზებები",
+            "Ttext44": "კომენტარები",
+            "Ttext45": "გამოწერა",
+            "Ttext46": "რეგისტრაცია"
+
+
+
+
+
+
+        }
+        
+};
+
+
+function changeLanguage(language) {
+    const translation = translations[language];
+  
+    Element1.textContent = translation.Ttext;
+    Element2.textContent = translation.Ttext1;
+    Element3.textContent = translation.Ttext2;
+    Element4.textContent = translation.Ttext3;
+    Element5.textContent = translation.Ttext4;
+    Element6.textContent = translation.Ttext5;
+    Element7.textContent = translation.Ttext6;
+    Element8.textContent = translation.Ttext7;
+    Element9.textContent = translation.Ttext8;
+    Element10.textContent = translation.Ttext9;
+    Element11.textContent = translation.Ttext10;
+    Element12.textContent = translation.Ttext11;
+    Element13.textContent = translation.Ttext12;
+    Element14.textContent = translation.Ttext13;
+    Element15.textContent = translation.Ttext14;
+    Element16.textContent = translation.Ttext15;
+    Element17.textContent = translation.Ttext16;
+    Element18.textContent = translation.Ttext17;
+    Element19.textContent = translation.Ttext18;
+    Element20.textContent = translation.Ttext19;
+    Element21.textContent = translation.Ttext20;
+    Element22.textContent = translation.Ttext21;
+    Element23.textContent = translation.Ttext22;
+    Element24.textContent = translation.Ttext23;
+    Element25.textContent = translation.Ttext24;
+    Element26.textContent = translation.Ttext25;
+    Element27.textContent = translation.Ttext26;
+    Element28.textContent = translation.Ttext27;
+    Element29.textContent = translation.Ttext28;
+    Element30.textContent = translation.Ttext29;
+    Element31.textContent = translation.Ttext30;
+    Element32.textContent = translation.Ttext31;
+    Element33.textContent = translation.Ttext32;
+    Element34.textContent = translation.Ttext33;
+    Element35.textContent = translation.Ttext34;
+    Element36.textContent = translation.Ttext35;
+    Element37.textContent = translation.Ttext36;
+    Element38.textContent = translation.Ttext37;
+    Element39.textContent = translation.Ttext38;
+    Element40.textContent = translation.Ttext39;
+    Element41.textContent = translation.Ttext40;
+    Element42.textContent = translation.Ttext41;
+    Element43.textContent = translation.Ttext42;
+    Element44.textContent = translation.Ttext43;
+    Element45.textContent = translation.Ttext44;
+    Element46.textContent = translation.Ttext45;
+    Element47.textContent = translation.Ttext46;
+}
+
+languageButtons.addEventListener('click', (e) => {
+    const lang = e.target.dataset.lang;
+    if (lang) {
+        changeLanguage(lang);
+    } 
+});
